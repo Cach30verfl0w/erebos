8d406adc633c52e034471491657edb88a76fec1c
Cach30verfl0w <cach30verfl0w@gmail.com>
2024-03-21 18:27:52 +0100

    Readd mimalloc

--- runtime/src/alloc.cpp
+++ runtime/src/alloc.cpp
@@ -14,7 +14,7 @@
 
 #include <mimalloc.h>
 
-/*
+
 void* operator new(size_t size) {
     return mi_malloc(size);
 }
@@ -22,4 +22,3 @@ void* operator new(size_t size) {
 void operator delete(void* ptr) {
     mi_free(ptr);
 }
- */
